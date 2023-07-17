0cfe721e6517f9fd9cefbff727a3e5429acc9579
Ignacio O <iopazog@gmail.com>
2023-07-17 13:57:23 +0200

    new componente Entry

---
+++ components/Entry.tsx
@@ -0,0 +1,33 @@
+import EntryInterface from '@/interfaces/EntryInterface';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+function Entry({ entry }: { entry: EntryInterface }) {
+  const router = useRouter();
+
+  const navigateToPodcaster = (id: string) => () => {
+    router.push(`/podcaster/${id}`);
+  };
+  return (
+    <li
+      key={entry.id}
+      onClick={navigateToPodcaster(entry.id)}
+      className="cursor-pointer"
+    >
+      <Image
+        className="mx-auto h-56 w-56 rounded-full"
+        src={entry.imageUrl}
+        alt="person image"
+        width={224}
+        height={224}
+        priority
+      />
+      <h3 className="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">
+        {entry.name}
+      </h3>
+      <p className="text-sm leading-6 text-gray-600">Author: {entry.artist}</p>
+    </li>
+  );
+}
+
+export default Entry;
